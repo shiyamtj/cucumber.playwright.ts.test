538e6b8f3b4a4a8e9feaade745ce9d668fa29687
shiyamtj <shiyamtj@users.noreply.github.com>
2025-12-29 10:15:26 +0000

    deploy: d78f732852f8e117b683ae4d6469de0d55946a7d

--- cucumber.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  default: {
-    requireModule: ['ts-node/register'],
-    require: ['src/config/env.ts', 'src/steps/**/*.ts'],
-    format: ['summary', 'progress-bar'],
-    paths: ['src/features/**/*.feature'],
-    parallel: 2,
-  },
-}
--- src/config/env.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-import path from 'path';
-
-// Load .env file
-dotenv.config({ path: path.resolve(process.cwd(), '.env') });
-// Load .env.local file, overriding .env variables
-dotenv.config({ path: path.resolve(process.cwd(), '.env.local'), override: true });
